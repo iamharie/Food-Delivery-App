770db9da0809eb56ea4cf71cd4b78b44ecf3a60f
iamharie <haranhari350@gmail.com>
2024-08-16 15:47:40 -0500

    added FoodItem component and modified FoodDisplay components; styling completed

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { StoreContext } from "../../context/StoreContext";
+import FoodItem from "../FoodItem/FoodItem";
 import "./FoodDisplay.css";
 
 export default function FoodDisplay({ category }) {
@@ -7,6 +8,20 @@ export default function FoodDisplay({ category }) {
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          return (
+            <FoodItem
+              key={index}
+              id={item._id}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+              image={item.image}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
